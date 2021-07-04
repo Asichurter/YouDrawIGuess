0f8a459fa2b8234ad47b06500111dbc047533e40
Asichurter <Asichurter@users.noreply.github.com>
2021-07-04 18:26:36 +0800

    remove useless old fields

--- server/core.py
+++ server/core.py
@@ -23,7 +23,7 @@ from vals.command import *
 
 class Server:
     def __init__(self):
-        self.MaxCont = config.game.MaxGamer
+
         self.GamerAccount = GamerAccount(config.server.UsrAccountAddr)
 
         # 创建套接字
@@ -33,45 +33,21 @@ class Server:
         # 绑定端口
         self.WelcomeSocket.bind(('', config.connect.ServerPort))
 
-        self.UnloggedGamers = []
-        self.Gamers = GamerGroup()
-        self.GameRoundPerGamer = 1      # 每一个玩家出题的循环次数
-        self.ServerMessage = ServerMessage()
-
-        self.TimerManager = TimerManager()
-
-
-        self.UsrSocket = []
-        self.UsrAddr = []
-        self.UsrName = []
-        self.UsrPoint = []
-        self.GamerCmdListenThreads = []
-        self.TimerId = {}
-        self.TimerThread = {}
-
-        self.GameLogic = None
-        self.Answer = None
-        self.Hint = None
-        self.PointPool = []
-        self.AnsweredGamer = []
-
-        self.CmdQueue = Queue()
-        self.GameBeginFlag = ThreadValue(True)
-        self.MessageLoopFlag = True
-
-    def recv_cmd(self, i, decode=True):
-        return recv_cmd(self.UsrSocket[i], decode)
+        self.MaxGamer = config.game.MaxGamer    # 最大游戏玩家数量
+        self.UnloggedGamers = []                # socket已连接，但是逻辑还没有登录的玩家
+        self.Gamers = GamerGroup()              # 已登录的玩家群组
+        self.GameRoundPerGamer = 1              # 每一个玩家出题的循环次数
+        self.ServerMessage = ServerMessage()    # 服务器端发送消息模板类
+        self.TimerManager = TimerManager()      # 服务器端计时器管理实例，可以方便地创建定时器
+        self.GamerCmdListenThreads = []         # 游戏状态消息循环监听的消息接受线程列表
+        self.GameLogic = None                   # 游戏逻辑实体类
+        self.CmdQueue = Queue()                 # 监听消息线程间通信队列
+        self.GameBeginFlag = ThreadValue(True)  # 指示游戏是否开始的标志
+        self.MessageLoopFlag = True             # 指示消息循环是否退出的标志
 
     def send_cmd_by_id(self, id_, command, **kwargs):
         gamer = self.Gamers.get_gamer_by_id(id_)
         gamer.send_cmd(command=command, **kwargs)
-
-    def send_cmd(self, i, command, **kwargs):
-        # logger.debug('server.send_cmd',
-        #              'send cmd: {}, vals: {}'
-        #              .format(command, kwargs))
-        self.Gamers[i].send_cmd(command, **kwargs)
-        # send_cmd(self.UsrSocket[i], command, **kwargs)
 
     def send_all_cmd(self, command, **kwargs):
         for g in self.Gamers:
@@ -93,10 +69,6 @@ class Server:
             t.start()
 
         self.game_state()
-
-    def initGameState(self):
-        self.fillPointPool()
-        self.AnsweredGamer.clear()
 
     def game_state(self):
         logger.debug('server.gamer_state',
@@ -141,174 +113,6 @@ class Server:
         # 关闭游戏
         self.close()
 
-    def game_state_(self):
-        print('entering game state')
-        for cur_gamer_index in range(self.GamerCount):
-            # 每开始一轮游戏，先调用初始化方法
-            self.initGameState()
-
-            # 向所有玩家发出新一轮游戏的指令来初始化
-            self.send_all_cmd(command='NewRound')
-
-            # 将画图者添加到已完成玩家列表中，防止其自己猜自己
-            self.AnsweredGamer.append(cur_gamer_index)
-
-            # 发送游戏提示
-            for i, s_ in enumerate(self.UsrSocket):
-                self.send_cmd(i, command='Inform',
-                              Content='现在由 {name} 画图'.format(name=self.UsrName[cur_gamer_index]))
-
-            # time.sleep(1)
-
-            # 向当前画图玩家发出开始画图的指令
-            self.send_cmd(cur_gamer_index, command='BeginPaint')
-
-            # # 接受画图玩家出的题
-            # cmd, vals = decode_msg(self.recv_cmd(cur_gamer_index))
-            # if cmd
-
-            print('thread starting from parent thread!')
-            while True:
-                msg = self.CmdQueue.get()  # 阻塞队列，处理接受到的命令
-                cmd, cmd_body = decode_msg(msg)
-                if cmd != '':
-                    logger.info('server.game',
-                                'cmd: {}, vals: {}'.format(cmd, cmd_body))
-
-                if cmd == 'BeginPaint':
-                    self.Answer = cmd_body['answer']
-                    self.Hint = cmd_body['hint']
-                    # 启动倒计时定时器
-                    self. \
-                        startTimer(timerId=0, downCount=int(config.game.RoundTime), interval=1)
-                    print('timer starting!')
-
-                elif cmd == 'Chat':
-                    usr_id = int(cmd_body['id'])
-                    content = cmd_body['content']
-
-                    if usr_id not in self.AnsweredGamer and self.checkAnswer(content):
-                        # 一旦玩家的答案正确
-                        print('用户%d的答案%s正确' % (usr_id, content))
-
-                        self.AnsweredGamer.append(usr_id)
-                        self.gamerScore(hid=cur_gamer_index, uid=usr_id)
-                        self.send_gamer_info()
-                        self.send_all_cmd(command='Chat',
-                                          Name='服务器',
-                                          Content='%s 已经猜对了答案' % self.UsrName[usr_id])
-                        print('游戏进度:', len(self.AnsweredGamer), len(self.UsrPoint))
-
-                        # 如果所有玩家都猜对了
-                        if len(self.AnsweredGamer) == len(self.UsrPoint):
-                            self.stopTimer(0)
-                            # 当前玩家停止画图
-                            self.send_cmd(cur_gamer_index, command='StopPaint')
-                            # 跳到下一个玩家
-                            break
-
-                    # 只有不是正确答案的聊天才会被公布
-                    else:
-                        self.send_all_cmd(command='Chat', **cmd_body)
-
-                elif cmd == 'Timeout':
-                    # for i in range(len(self.UsrSocket)):
-                    #     self.send_cmd(i, command='Inform', Content='时间到')
-                    self.send_all_cmd(command='Inform', Content='时间到')
-                    self.send_cmd(cur_gamer_index, command='StopPaint')
-                    time.sleep(2)
-                    break
-
-                elif cmd == 'PaintPoint' or \
-                        cmd == 'ClickPoint':
-                    for i_, s_ in enumerate(self.UsrSocket):
-                        if i_ != cur_gamer_index:
-                            self.send_cmd(i_, command=cmd, **cmd_body)
-
-                elif cmd == 'TimerEvent':
-                    for i_, s_ in enumerate(self.UsrSocket):
-                        self.send_cmd(i_, command=cmd, **cmd_body)
-
-                elif cmd == 'SettingChanged':
-                    for i_, s_ in enumerate(self.UsrSocket):
-                        if i_ != cur_gamer_index:
-                            self.send_cmd(i_, command=cmd, **cmd_body)
-
-        self.send_all_cmd(command='EndGame')
-
-    def startTimer(self, timerId, downCount=None, interval=1, eps=5e-2):
-        def newTimer():
-            self.TimerId[timerId] = True
-            delta = 1 if downCount is None else -1
-            scs = 0 if downCount is None else int(downCount)
-            flag = (downCount is None) or (scs > 0)  # 对于倒计时时钟，是否时钟尚存
-            print('timer starting: ', timerId)
-
-            while self.TimerId[timerId] and flag:
-                time.sleep(interval - eps)
-
-                # msg = b'TimerEvent\nSecond %d' % scs
-                msg, _ = encode_msg(command='TimerEvent',
-                                    second=str(scs))
-                self.CmdQueue.put(msg)
-
-                flag = (downCount is None) or (scs > 0)
-                scs = scs + delta
-
-            # 对于倒计时时钟，时间到的时候会放入一条时间到的指令
-            if not flag:
-                # msg = b'Timeout'
-                msg, _ = encode_msg(command='Timeout')
-                self.CmdQueue.put(msg)
-
-            del self.TimerId[timerId]
-            del self.TimerThread[timerId]
-            print('timer %d exit' % timerId)
-
-        assert timerId not in self.TimerId, '计时器ID已存在！'
-
-        self.TimerThread[timerId] = threading.Thread(target=newTimer, args=(), daemon=True)
-        self.TimerThread[timerId].start()
-
-    def stopTimer(self, timerId):
-        self.TimerId[timerId] = False
-        # 等待0.5秒计时器进程退出
-        time.sleep(0.5)
-
-    def login_thread(self, socket_obj, con_addr):
-        socket_obj.settimeout(None)
-        # 先出于等待登录状态
-        while True:
-            cmd, vals = recv_cmd(socket_obj, decode=True)
-            if cmd == 'Login':
-                try:
-                    user_name = vals['Username']
-                    password = vals['Password']
-                    passed, login_info = self.check_login(user_name, password)
-                except KeyError:
-                    logger.error('server.login',
-                                 'Login指令中缺少 username '
-                                 '和 password 项！')
-                    continue
-                send_cmd(socket_obj,
-                         command='Login',
-                         LoginStateCode=1 if passed else 0,
-                         LoginMessage=login_info,
-                         ID=id)
-
-                if passed:
-                    logger.info('server.login',
-                                'sending gamer info...')
-
-                    self.add_gamer(socket_obj, con_addr, user_name)
-                    # todo: 修改为Gamer实现
-                    self.send_gamer_info()
-                    return
-
-            # 玩家主动或者被动放弃登录
-            elif cmd == 'ExitLogin':
-                return
-
     def handle_host_begin_game_cmd(self, host_index=0):
         logger.debug('handle_host_cmd',
                      'host handling threading start!')
@@ -329,7 +133,7 @@ class Server:
     def login_state(self):
         # 欢迎socket监听3秒就开始监听主机命令
         self.WelcomeSocket.settimeout(config.server.ServerAcceptInterval)
-        self.WelcomeSocket.listen(self.MaxCont)
+        self.WelcomeSocket.listen(self.MaxGamer)
 
         login_threads = []
         # 外层循环接受玩家连接
@@ -386,27 +190,6 @@ class Server:
             except socket.timeout:
                 pass
         return None
-
-    # def send_gamer_info(self):
-    #     gamer_info = self.get_gamer_info()
-    #     self.send_all_cmd(**make_gamer_info_command(gamer_info))
-
-    def get_gamer_info(self):
-        gamers = []
-        for g in self.Gamers:
-            gamers.append((g.UserName, g.Point))
-        return gamers
-
-    def checkAnswer(self, answer):
-        return self.Answer == answer
-
-    def fillPointPool(self):
-        # todo: 计分方式有点问题
-        self.PointPool = config.game.GuessPointPool
-
-    def gamerScore(self, hid, uid):
-        self.UsrPoint[hid] += config.game.DrawPoint
-        self.UsrPoint[uid] += self.PointPool.pop(0)
 
     def close(self):
         self.send_all_cmd(**make_end_game_command())
