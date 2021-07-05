9c1df9cac71bd03a7230be1cb1ae4818dbc1310d
Asichurter <Asichurter@users.noreply.github.com>
2021-07-05 13:22:07 +0800

    fix encoding bug of break msg loop cmd, server msg and entity refactoring

--- server/handlers/game_handlers.py
+++ server/handlers/game_handlers.py
@@ -1,6 +1,7 @@
 import config
 from vals.command import *
 from vals.timer import *
+from vals.entity import SERVER_CHAT_NAME
 from log import GlobalLogger as logger
 from com.protocol import encode_msg
 
@@ -33,9 +34,10 @@ def handle_game_chat(server, **kwargs):
     if is_answered:
         logger.info('game_handlers.handle_game_chat',
                     f'gamer {name} has answered the question')
+        server_chat_msg = server.ServerMessage.make_gamer_answered_inform_message(name)
         server.send_all_cmd(**make_chat_command(id_=-1,
-                                                name='服务器',
-                                                content=f'{name}已经猜对了答案'))
+                                                name=SERVER_CHAT_NAME,
+                                                content=server_chat_msg))
         # 刷新分数
         server.send_all_cmd(**make_gamer_info_command(server.Gamers.pack_all_gamers_info()))
     # 否则将chat视为一般的聊天，广播给所有玩家
@@ -60,7 +62,9 @@ def handle_game_chat(server, **kwargs):
                     server.GameLogic.Answer)))
         # 3. 设置flag终止消息监听循环，同时放入一条信息刷新循环判断
         server.MessageLoopFlag = False
-        server.CmdQueue.put(encode_msg(**make_break_message_loop_command()))
+        # 只取解码信息的body，不取包含body大小的头部信息，因为是本地的直接指令存入
+        break_msg_loop_cmd, _ = encode_msg(**make_break_message_loop_command())
+        server.CmdQueue.put(break_msg_loop_cmd)
         # 4. 将答案置为无效，防止其他玩家在谜底揭晓处于无效状态时进行答题
         server.GameLogic.set_answer_valid_state(False)
 
